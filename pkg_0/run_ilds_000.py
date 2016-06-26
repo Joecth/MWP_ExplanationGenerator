28a9f955c1a102234a1c1ab1f3e91725a3488b2c
Joecth <jdreamonion2@gmail.com>
2016-06-27 07:13:25 +0800

    Reg : Making up the missed file

--- pkg_0/run_ilds_000.py
+++ pkg_0/run_ilds_000.py
@@ -4,22 +4,23 @@ import os
 import popen2
 
 def foo(idx):
-    os.system('./run_uwcases_{}.py >& uw{}.log'.format(idx, idx))
-    #print ("diff -q uwds{}_golden_000.log uw{}.log".format(idx, idx))
-    is_differ = os.system("diff -q uwds{}_golden_000.log uw{}.log".format(idx, idx))
+    os.system('./run_ilds_{}.py >& il{}.log'.format(idx, idx))
+    #print ("diff -q ilds{}_golden_000.log il{}.log".format(idx, idx))
+    is_differ = os.system("diff -q ilds{}_golden_000.log il{}.log".format(idx, idx))
     err_log = ""
 
     if is_differ:
         print("Check Reg Fail!")
-        print("Please diff uwds{}_golden_000.log uw{}.log\n".format(idx, idx))
+        print("Please diff ilds{}_golden_000.log il{}.log\n".format(idx, idx))
     else:
         print("Reg%s Pass!\n" % idx)
 
 if __name__ == '__main__':
-    #stdout, stdin = popen2.popen2('./run_uwcases_1.py')
+    #stdout, stdin = popen2.popen2('./run_ilds_1.py')
     #foo(3)
-    for tmp in range(3):
-        idx = tmp + 1
-        print ('Dir%s :::'% idx)
+    #foo(1)
+    for tmp in range(1):
+        idx = tmp + 4 
+        print ('Dir mini_%s :::'% idx)
         foo(idx)
 
